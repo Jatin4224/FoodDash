80a7cf051f581d9f28855737f7edbce35d0925f0
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-26 17:13:46 +0530

    userClass component✔️

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,14 @@
-const User = () => {
+import { useState } from "react";
+
+const User = ({ name }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
-      <h2>Name: Jatin Sharma</h2>
-      <h3>Location : kota</h3>
+      {/* <h1>Count = {count}</h1>
+      <h1>Count2 = {count2}</h1>
+      <h2>Name: {name}</h2>
+      <h3>Location : kota</h3> */}
       <h3>contact : @jatin4224</h3>
     </div>
   );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
   render() {
+    const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
-        <h2>Name: Jatin Sharma</h2>
-        <h3>Location : kota</h3>
+        <h1>count : {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count Increase
+        </button>
+        <h2>Name:{name}</h2>
+        <h3>Location : {location}</h3>
         <h3>contact : @jatin4224</h3>
       </div>
     );
