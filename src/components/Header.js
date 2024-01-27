8e14d95859caf10947ee7e6608a53af2e8cc23b9
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-28 01:54:51 +0530

    Implemented lazy loading for improved performance

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,6 +26,9 @@ const Header = () => {
           <li>
             <Link to="/contact">Contact Us</Link>
           </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
           <li>cart</li>
 
           <button
