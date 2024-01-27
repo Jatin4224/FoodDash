3d85b1bbc08983fd0fb3a0244c564bed5c7233de
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-28 03:00:13 +0530

    ui updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,6 +9,7 @@ const Body = () => {
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
+  console.log(listOfRestaurants);
   useEffect(() => {
     fetchData();
   }, []);
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -10,16 +10,23 @@ const RestaurantCard = (props) => {
     avgRating,
     costForTwo,
     deliveryTime,
+    sla,
+    locality,
   } = resData?.info;
   return (
     <div className="res-card">
-      <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>⭐{avgRating}</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{deliveryTime}</h4>
-      {/* <h4>{sla.slaString}</h4> */}
+      <img
+        className="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+        alt="Restaurant Logo"
+      />
+      <h3 className="res-name">{name}</h3>
+      <h4 className="res-cuisines">{cuisines.join(", ")}</h4>
+      <h4 className="res-rating">avgRating:⭐{avgRating}</h4>
+      <h4 className="res-cost">Cost for Two: {costForTwo}</h4>
+      <h4 className="res-delivery-time">Delivery Time: {deliveryTime}</h4>
+      <h4 className="res-sla">SLA: {sla.slaString}</h4>
+      <h4 className="res-locality">Locality: {locality}</h4>
     </div>
   );
 };
