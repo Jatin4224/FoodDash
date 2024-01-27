9809accbd248cc53a1045a432172c89860c395d6
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-27 22:46:05 +0530

    fixed error

--- src/components/Body.js
+++ src/components/Body.js
@@ -74,7 +74,7 @@ const Body = () => {
             {filteredRestaurant.map((restaurant) => (
               <Link
                 key={restaurant.info.id}
-                to={"/restaurants" + restaurant.info.id}
+                to={"/restaurants/" + restaurant.info.id}
               >
                 <RestaurantCard resData={restaurant} />
               </Link>
