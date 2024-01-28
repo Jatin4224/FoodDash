f091631ef97ae51033906082a4c92a8d9e29d25c
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-01-29 04:19:15 +0530

    .

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import ItemList from "./itemList";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
   return (
     <div>
       <div className="w-8/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between" onClick={handleClick}>
           <span className="font-bold text-lg">{data.title}</span>
 
           <span>⬇️</span>
         </div>
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
     </div>
   );
