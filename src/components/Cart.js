9dcfaec9ef4f02254584ad41758e2974cd958291
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-11 00:17:59 +0530

    feature : clearcart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,28 @@
 import React from "react";
+import ItemList from "./ItemList";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
+  //how to read cart from the store - subcribe
+  const cartItems = useSelector((store) => store.cart.items);
+
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
-    <div className="text-center m-10 p-10">
+    <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>
+      <div className="w-4/12 text-center m-4 p-4">
+        <button
+          className="p-2 m-2 bg-black text-white rounded-lg "
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+        <ItemList items={cartItems} />
+      </div>
     </div>
   );
 };
