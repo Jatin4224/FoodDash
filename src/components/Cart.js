5704e8a17dd63fa85e0b5786175bd1215211e628
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-11 01:21:43 +0530

    .

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,24 +4,39 @@ import { useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
-  //how to read cart from the store - subcribe
   const cartItems = useSelector((store) => store.cart.items);
-
   const dispatch = useDispatch();
+
   const handleClearCart = () => {
     dispatch(clearCart());
   };
+
   return (
-    <div className="text-center m-4 p-4">
-      <h1 className="text-2xl font-bold">Cart</h1>
-      <div className="w-4/12 text-center m-4 p-4">
-        <button
-          className="p-2 m-2 bg-black text-white rounded-lg "
-          onClick={handleClearCart}
-        >
-          Clear Cart
-        </button>
-        <ItemList items={cartItems} />
+    <div className="text-center mx-auto max-w-lg p-4">
+      <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+
+      <div className="bg-white rounded-lg shadow-md overflow-hidden">
+        <div className="flex items-center justify-between px-4 py-3 bg-gray-200">
+          <h1 className="text-grey font-bold">Items in Cart</h1>
+          <button
+            className="px-4 py-2 bg-red-600 text-grey font-semibold rounded-lg hover:bg-grey-500"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        </div>
+
+        {cartItems.length === 0 && (
+          <div className="p-4 text-center text-gray-600">
+            Your cart is empty. Start adding items!
+          </div>
+        )}
+
+        {cartItems.length > 0 && (
+          <div className="divide-y divide-gray-200">
+            <ItemList items={cartItems} />
+          </div>
+        )}
       </div>
     </div>
   );
