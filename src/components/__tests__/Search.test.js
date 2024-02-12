33b175065bab38aa8f7d8dab0252ea5ea6ee5719
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-13 01:13:29 +0530

    integration testingting

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,26 @@
+import { render, screen, act } from "@testing-library/react";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/mockResListData.json";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+
+it("should render the Body component with Search", async () => {
+  await act(async () => {
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    );
+  });
+
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  expect(searchBtn).toBeInTheDocument();
+});
