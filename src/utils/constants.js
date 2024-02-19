dd70d15a4bd1c3ef8a3eee856051f413de2d7ecb
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-02-19 22:59:48 +0530

    Bug fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,4 +6,7 @@ export const LOGO_URL =
   "https://pbs.twimg.com/media/F71LsL8W4AA9faj?format=jpg&name=900x900";
 
 export const MENU_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.3260152&lng=75.57618289999999&restaurantId=";
+  "https://corsproxy.org/?" +
+  encodeURIComponent(
+    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=31.3260152&lng=75.57618289999999&restaurantId="
+  );
